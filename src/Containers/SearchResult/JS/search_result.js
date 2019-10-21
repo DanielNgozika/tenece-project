ebecdcdea5100061e4e591bf266d3c1476e00395
Daniel Ngozika <danielngozikac@gmail.com>
2019-10-21 11:37:32 +0100

    Create search result page.

---
+++ src/Containers/SearchResult/JS/search_result.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from "../CSS/search_result.module.css";
+
+class SearchResultPage extends Component {
+    render() {
+        return (
+            <main className={styles.main}>
+                
+            </main>
+        )
+    }
+}
+
+export default SearchResultPage;
