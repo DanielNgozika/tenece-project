034587ba397d9b3194eea6540dcbdafc0737f85f
Daniel Ngozika <danielngozikac@gmail.com>
2019-10-21 13:41:11 +0100

    Create each result component.

---
+++ src/Components/SearchResult/JS/each_result.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "../CSS/each_result.module.css";
+
+const eachCountry = ({ country }) => (
+	<div className={styles.div}>
+		<div className={styles.name}>
+			<span>Country:</span>
+			<span>{country.name}</span>
+		</div>
+		<div>
+			<span>Region:</span>
+			<span>{country.region}</span>
+		</div>
+		<div>
+			<span>Population:</span>
+			<span>{country.population}</span>
+		</div>
+		<div>
+			<span>Timezone:</span>
+			<span>{country.timezones[0]}</span>
+		</div>
+		<div>
+			<span>Currency:</span>
+			<span>{country.currencies[0].name}</span>
+		</div>
+		<div>
+			<span>Languages:</span>
+			<span>{country.languages[0].name}</span>
+		</div>
+		<div>
+			<span>Calling codes:</span>
+			<span>{country.callingCodes[0]}</span>
+		</div>
+	</div>
+);
+
+export default eachCountry;
