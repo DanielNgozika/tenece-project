a0be1da50944c69bf1670209daddb56e00b43a2d
Daniel Ngozika <danielngozikac@gmail.com>
2019-10-21 11:39:14 +0100

    Create search result route.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
 import Home from "./Containers/Home/JS/home";
+import SearchResultPage from "./Containers/SearchResult/JS/search_result";
 
 const App = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" component={Home} />
-        </Switch>
-    </BrowserRouter>
-)
+	<BrowserRouter>
+		<Switch>
+			<Route path="/search_result" component={SearchResultPage} />
+			<Route path="/" component={Home} />
+		</Switch>
+	</BrowserRouter>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
